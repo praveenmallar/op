7a374a6bd213ebf923e3c8248fc593fa03f32c7b
praveen gopinath <praveenmallar@gmail.com>
2018-05-05 09:48:22 +0530

    temp

--- room.py
+++ room.py
@@ -21,26 +21,47 @@ class Room (Frame):
 		self.delete.pack(side=LEFT,padx=20)
 		f=Frame(self,bd=1,relief=RIDGE,padx=10,pady=10)
 		f.pack(pady=20,padx=20)
-		self.editbox=Entry(f)
-		self.editbox.pack(expand=1,pady=10)
+		self.editbox=StringVar()
+		Entry(f,textvariable=self.editbox).pack(expand=1,pady=10)
 		self.add=Button(f,text="Add",command=self.add)
 		self.add.pack(side=LEFT,padx=20)
 		self.reset=Button(f,text="Reset",command=self.reset)
 		self.reset.pack(side=LEFT,padx=20)
-		self.editroom=None
 		self.packitems()
 		
 	def packitems(self):
-		pass
+		cur=cdb.Db().connection().cursor()
+		sql="select * from room order by cast(room_num as unsigned), room_num;"
+		cur.execute(sql)
+		rows=cur.fetchall()
+		rooms=[]
+		for r in rows:
+			rooms.append([r[1],r[0]])
+		self.rooms.changelist(rooms)
+		self.editroom=None
 		
 	def add(self):
-		pass
+		room=self.editbox.get()
+		if self.editroom==None:
+			sql="insert into room (room_num) values (%s);"
+			values=[room]
+		else:
+			sql="update room set room_num=%s where id=%s;"
+			values=[room,self.editroom]
+		con=cdb.Db().connection()
+		cur=con.cursor()
+		cur.execute(sql,values)
+		con.commit()
+		self.packitems()
+		self.editbox.set("")		
 		
 	def reset(self):
 		pass
 		
 	def edit(self):
-		pass
+		room=self.rooms.get()
+		self.editroom=room[1]
+		self.editbox.set(room[0])
 		
 	def delete(self):
 		pass
